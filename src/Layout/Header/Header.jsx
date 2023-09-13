c795f5125f363ab2e615ed66ce4526d93f956639
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-09-13 11:46:10 +0100

    a

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,7 +1,9 @@
+import { HeaderContainer } from "./Header.styled"
+
 const Header=()=>{
-    return (<>
+    return (<HeaderContainer>
                 <h1>Header</h1>
-            </>
+            </HeaderContainer>
         
     )
 }
---
+++ src/Layout/Header/Header.styled.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const HeaderContainer=styled.div`
+background-color:grey;
+`
