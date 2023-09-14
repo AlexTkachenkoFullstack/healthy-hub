46196c2a8b65ab96299b09fdfda040b5a00c9d7b
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-09-14 20:49:58 +0100

    Update PrivateRoute.jsx

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -5,7 +5,7 @@ const PrivateRoute = ({ redirectTo='/', component }) => {
     // треба перевірити чи авторизований користувач(чи є токен у глобальному стейті) 
 
     // змінити умову для авторизованого чи неавторизованого користувача(!auth)
-    const shouldRedirect = true; //замінити на значення зі стейту
+    const shouldRedirect = false; //замінити на значення зі стейту
     return (<>
     {shouldRedirect ? <Navigate to={redirectTo} />  : component}
     </>    
