247cac916b64f96b9123876e6c765eeeefe492ee
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-09-16 17:07:47 +0300

    styled

--- src/components/SignUpForm/SignUpFirst/SignUpFirst.jsx
+++ src/components/SignUpForm/SignUpFirst/SignUpFirst.jsx
@@ -1,4 +1,19 @@
 import React, { useState } from 'react';
+import image from '../../../assets/images/sport-and-fitness-tracker.png';
+import {
+  SignUpFirstContainer,
+  Image,
+  MainHeader,
+  Text,
+  InputBox,
+  InputText,
+  InputButton,
+  Form,
+  TextInEnd,
+  SignInText,
+  FinishBlock,
+  QuestionForm,
+} from './SignUpFirst.styled';
 
 const SignUpFirst = ({ goNext }) => {
   const [formData, setFormData] = useState({
@@ -20,56 +35,61 @@ const SignUpFirst = ({ goNext }) => {
       // тут буде логіка відправки форми на сервер
     };
     
-  return (
-    <>
-      <h1>Sign up</h1>
-      <p>You need to register to use the service</p>
+    return (
+      <SignUpFirstContainer>
+        <Image src={image} alt="Sport and fitness tracker" />
+        <QuestionForm><MainHeader>Sign up</MainHeader>
+        <Text>You need to register to use the service</Text>
 
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name" />
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email" />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="E-mail"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password" />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" onClick={goNext}>
-          Sign Up
-        </button>
-      </form>
-      <p>Do you already have an account?</p>
-      <div>Sign in</div>
-    </>
-  );
+        <Form onSubmit={handleSubmit}>
+          <InputBox>
+            <label htmlFor="name" />
+            <InputText
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </InputBox>
+          <InputBox>
+            <label htmlFor="email" />
+            <InputText
+              type="email"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </InputBox>
+          <InputBox>
+            <label htmlFor="password" />
+            <InputText
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </InputBox>
+          <InputButton type="submit" onClick={goNext}>
+            Sign Up
+          </InputButton>
+        </Form>
+        <FinishBlock>
+          <TextInEnd>Do you already have an account?</TextInEnd>
+          <SignInText>Sign in</SignInText>
+                </FinishBlock>
+            </QuestionForm>
+        
+      </SignUpFirstContainer>
+    );
 };
 
 export default SignUpFirst;
--- src/components/SignUpForm/SignUpFirst/SignUpFirst.styled.jsx
+++ src/components/SignUpForm/SignUpFirst/SignUpFirst.styled.jsx
@@ -1,1 +1,148 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+
+export const SignUpFirstContainer = styled.div`
+  background-color: var(--bg-primary);
+  padding: 24px 0;
+
+  /* потім margin прибрати*/
+  margin-top: 60px;
+
+  @media (min-width: 834px) {
+    padding: 40px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media (min-width: 1440px) {
+    display: flex;
+    flex-direction: row;
+    gap: 104px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Image = styled.img`
+  width: 300px;
+  height: 296px;
+  margin-bottom: 24px;
+  @media (min-width: 834px) {
+    width: 380px;
+    height: 376px;
+    margin-bottom: 40px;
+  }
+  @media (min-width: 1440px) {
+    width: 592px;
+  height: 588px;
+  margin-left:150px;
+  }
+  
+`;
+
+export const MainHeader = styled.h1`
+  color: var(--text-color-primary-white);
+  font-family: Poppins;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 1.25;
+  margin-bottom: 24px;
+  @media (min-width: 834px) {
+    font-size: 30px;
+    line-height: 1.2;
+  }
+`;
+
+export const Text = styled.p`
+  color: var(--text-color-secondary-grey);
+  font-family: Poppins;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.33;
+  margin-bottom: 24px;
+  @media (min-width: 834px) {
+    font-size: 22px;
+    line-height: 1.44;
+  }
+`;
+
+export const InputBox = styled.div`
+  border-radius: 12px;
+  border: 1px solid var(--text-color-active-page-green);
+  background: var(--bg-secondary);
+`;
+
+export const InputText = styled.input`
+  color: var(--text-color-secondary-grey);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.429;
+  background: transparent;
+  border: none;
+  outline: none;
+  padding: 10px 8px;
+`;
+
+export const InputButton = styled.button`
+  border-radius: 12px;
+  border: none;
+  outline: none;
+  background: var(--text-color-active-page-green);
+  width: 300px;
+  height: 36px;
+
+  color: var(--bg-primary);
+  text-align: center;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.43;
+
+  @media (min-width: 834px) {
+    width: 380px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  margin-bottom: 80px;
+`;
+
+export const TextInEnd = styled.p`
+  color: var(--text-color-secondary-grey);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.43;
+`;
+
+export const SignInText = styled.div`
+  color: var(--text-color-primary-white);
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.43;
+`;
+
+export const FinishBlock = styled.div`
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  @media (min-width: 834px) {
+   flex-direction:row;
+  }
+`;
+
+export const QuestionForm = styled.div`
+  
+`;
