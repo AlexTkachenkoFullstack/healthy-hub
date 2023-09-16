d7d16db7edf0a600d2a4f6d12af4bd154993fd3e
petekostrytsya <petekostrytsya@gmail.com>
2023-09-16 15:48:17 +0300

    add pages RecommendedFood

--- src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
+++ src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
@@ -1,8 +1,53 @@
-const RecommendedFoodPage=()=>{
-    return (<>
-                <h2>RecommendedFoodPage</h2>
-            </>
-    )
-}
+import React from 'react';
 
-export default RecommendedFoodPage
+const RecommendedFoodPage = ({ title, image, recommendedProducts }) => {
+  return (
+    <div>
+       <h3>{title}</h3>
+        <img src={image} alt="Recommended Food" style={{ maxWidth: '100%' }} />
+      <ul>
+        {recommendedProducts.map((product, index) => (
+          <li key={index}>{product}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecommendedFoodPage;
+
+
+// import React, { useState, useEffect } from 'react';
+// import RecommendedFoodPage from './RecommendedFoodPage';
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     fetch('your_data.json')
+//       .then((response) => response.json())
+//       .then((jsonData) => {
+//         setData(jsonData);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching data:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       {data && (
+//         <RecommendedFoodPage
+//           title={data.title}
+//           image={data.image}
+//           recommendedProducts={data.recommendedProducts}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+
+
+
+
