418e3afc458399973d36bd69d2cdf9bb6514cb1e
barkode <konstantinks@gmail.com>
2023-09-19 00:53:36 +0100

    Update YourGoal.jsx

--- src/components/SignUpForm/YourGoal/YourGoal.jsx
+++ src/components/SignUpForm/YourGoal/YourGoal.jsx
@@ -1,6 +1,6 @@
 import { Formik, Form, Field } from 'formik';
 import image from '../../../assets/images/summer-hiking.png';
-import "./YourGoal.css";
+import './YourGoal.css';
 import {
   YourGoalContainer,
   Image,
@@ -12,37 +12,37 @@ import {
   BlockButton,
 } from './YourGoal.styled';
 
-const YourGoal = ({ goNext, goBack }) => {
+const YourGoal = ({ goNext, data }) => {
+  const handleSubmit = (values, actions) => {
+    goNext(values);
+  };
+
   return (
-    <Formik>
-      {() => (
-        <YourGoalContainer>
-          <Image src={image} alt="Summer hinking" />
-          <Form>
-            <YourGoalHeader id="your-goal-group">Your Goal</YourGoalHeader>
-            <Text>Choose a goal so that we can help you effectively</Text>
-            <LabelBlock role="group" aria-label="your-goal-group">
-              <Label>
-                <Field type="radio" name="Your Goal" value="Lose fat" />
-                Lose fat
-              </Label>
-              <Label>
-                <Field type="radio" name="Your Goal" value="Maintain" />
-                Maintain
-              </Label>
-              <Label>
-                <Field type="radio" name="Your Goal" value="Gain Muscle" />
-                Gain Muscle
-              </Label>
-            </LabelBlock>
-            <BlockButton>
-              <InputButton type="submit" onClick={goNext}>
-                Next
-              </InputButton>
-            </BlockButton>
-          </Form>
-        </YourGoalContainer>
-      )}
+    <Formik initialValues={data} onSubmit={handleSubmit}>
+      <YourGoalContainer>
+        <Image src={image} alt="Summer hinking" />
+        <Form>
+          <YourGoalHeader id="your-goal-group">Your Goal</YourGoalHeader>
+          <Text>Choose a goal so that we can help you effectively</Text>
+          <LabelBlock role="group" aria-label="your-goal-group">
+            <Label>
+              <Field type="radio" name="goal" value="Lose fat" />
+              Lose fat
+            </Label>
+            <Label>
+              <Field type="radio" name="goal" value="Maintain" />
+              Maintain
+            </Label>
+            <Label>
+              <Field type="radio" name="goal" value="Gain Muscle" />
+              Gain Muscle
+            </Label>
+          </LabelBlock>
+          <BlockButton>
+            <InputButton type="submit">Next</InputButton>
+          </BlockButton>
+        </Form>
+      </YourGoalContainer>
     </Formik>
   );
 };
