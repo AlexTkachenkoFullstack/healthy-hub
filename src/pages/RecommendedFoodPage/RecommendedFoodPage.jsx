21195ac1a0b59fa14fbb72b98b20f82a2ff89431
petekostrytsya <petekostrytsya@gmail.com>
2023-09-19 22:20:38 +0300

    Update ReccomFood

--- src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
+++ src/pages/RecommendedFoodPage/RecommendedFoodPage.jsx
@@ -1,19 +1,22 @@
 import { RecommendedFood, Title, DesctopContainer, PhotoPage, ProductsList, ProductItem } from "./RecommendedFoodPage.styled";
 
+import recommendedFood from 'RecommendedFood.json';
+import { randomArray } from 'utils';
 
 const RecommendedFoodPage = () => {
-  const recommendedProducts = [
-    { id: 1, name: 'Product 1' },
-    { id: 2, name: 'Product 2' },
-    { id: 3, name: 'Product 3' },
-    { id: 4, name: 'Product 4' },
-    { id: 5, name: 'Product 5' },
-    { id: 6, name: 'Product 6' },
-    { id: 7, name: 'Product 7' },
-    { id: 8, name: 'Product 8' },
-    { id: 9, name: 'Product 9' },
-    { id: 10, name: 'Product 10' }
-  ];
+  const recommendedProducts = randomArray(recommendedFood, 10);
+  // const recommendedProducts = [
+  //   { id: 1, name: 'Product 1' },
+  //   { id: 2, name: 'Product 2' },
+  //   { id: 3, name: 'Product 3' },
+  //   { id: 4, name: 'Product 4' },
+  //   { id: 5, name: 'Product 5' },
+  //   { id: 6, name: 'Product 6' },
+  //   { id: 7, name: 'Product 7' },
+  //   { id: 8, name: 'Product 8' },
+  //   { id: 9, name: 'Product 9' },
+  //   { id: 10, name: 'Product 10' }
+  // ];
 
   return (
     <RecommendedFood>
