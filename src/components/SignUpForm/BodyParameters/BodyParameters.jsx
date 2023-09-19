8516653a44081fba4cae3085d0dca85e82f1d0b3
barkode <konstantinks@gmail.com>
2023-09-19 01:17:31 +0100

    Update BodyParameters.jsx

--- src/components/SignUpForm/BodyParameters/BodyParameters.jsx
+++ src/components/SignUpForm/BodyParameters/BodyParameters.jsx
@@ -1,5 +1,5 @@
 import { Formik, Form } from 'formik';
-import image from '../../../assets/images/body-contouring.png'
+import image from '../../../assets/images/body-contouring.png';
 import {
   BodyParametersContainer,
   Image,
@@ -12,38 +12,38 @@ import {
   BackButton,
 } from './BodyParameters.styled';
 
-const BodyParameters = ({ goNext, goBack }) => {
+const BodyParameters = ({ goNext, goBack, data }) => {
+  const handleSubmit = (values, actions) => {
+    goNext(values);
+  };
+
   return (
     <BodyParametersContainer>
       <Image src={image} alt="Illustration Body parameters" />
-      <Formik>
-        {() => (
-          <Form>
-            <BodyParametersHeader>Body parameters</BodyParametersHeader>
-            <Text>
-              Enter your parameters for correct performance tracking
-            </Text>
-            <ChooseText>Height</ChooseText>
-            <InputBox>
-              <InputText name="Height" placeholder="Enter your height" />
-            </InputBox>
-            <ChooseText>Weight</ChooseText>
-            <InputBox>
-              <InputText name="Weight" placeholder="Enter your weight" />
-            </InputBox>
-            {/* <Field name="Weight" /> */}
-            <p>
-              <InputButton type="submit" onClick={goNext}>
-                Next
-              </InputButton>
-            </p>
-            <p>
-              <BackButton type="button" onClick={goBack}>
-                Back
-              </BackButton>
-            </p>
-          </Form>
-        )}
+      <Formik initialValues={data} onSubmit={handleSubmit}>
+        <Form>
+          <BodyParametersHeader>Body parameters</BodyParametersHeader>
+          <Text>Enter your parameters for correct performance tracking</Text>
+          <ChooseText>Height</ChooseText>
+          <InputBox>
+            <InputText name="Height" placeholder="Enter your height" />
+          </InputBox>
+          <ChooseText>Weight</ChooseText>
+          <InputBox>
+            <InputText name="Weight" placeholder="Enter your weight" />
+          </InputBox>
+          {/* <Field name="Weight" /> */}
+          <p>
+            <InputButton type="submit" onClick={goNext}>
+              Next
+            </InputButton>
+          </p>
+          <p>
+            <BackButton type="button" onClick={goBack}>
+              Back
+            </BackButton>
+          </p>
+        </Form>
       </Formik>
     </BodyParametersContainer>
   );
