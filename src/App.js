b4200241a5f306bb21ff1736106381bc1b783745
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-09-20 13:16:46 +0100

    package

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAuthStatus } from './redux/auth/selectors';
-import { instance, loginThunk, refreshThunk, registrationThunk } from './redux/auth/operations';
+import { refreshThunk } from './redux/auth/operations';
 
 function App() {
 
