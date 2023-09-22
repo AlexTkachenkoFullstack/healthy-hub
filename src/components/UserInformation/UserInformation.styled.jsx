e960ea31e0fa1f182db98b842e9e8e7b590a39ad
Nadiia Tsytsylina <miniova95@gmail.com>
2023-09-22 17:02:23 +0300

    update UserSettingsForm

--- src/components/UserInformation/UserInformation.styled.jsx
+++ src/components/UserInformation/UserInformation.styled.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { Field, Form } from 'formik';
 
-export const Form = styled.form`
+export const StyledForm = styled(Form)`
   display: flex;
   flex-direction: column;
   gap: 40px;
@@ -25,13 +26,14 @@ export const Label = styled.label`
   font-size: 14px;
   line-height: 1.43;
   font-weight: 500;
+  width: 100%;
 
   @media (min-width: 834px) {
     width: 219px;
   }
 `;
 
-export const Input = styled.input`
+export const Input = styled(Field)`
   padding: 8px 10px;
   background-color: transparent;
   color: var(--text-color-primary-white);
@@ -40,6 +42,8 @@ export const Input = styled.input`
   border-radius: 12px;
   border: 1px solid var(--text-color-secondary-grey);
   outline: none;
+  border: ${({ borderstyle }) => borderstyle};
+  width: 100%;
 
   @media (min-width: 834px) {
     width: 212px;
@@ -53,8 +57,28 @@ export const RadioContainer = styled.div`
 `;
 
 export const RadioLabel = styled.label`
-  font-size: 14px;
-  line-height: 1.43;
+  display: grid;
+  grid: repeat(1, auto) / auto-flow;
+  grid-auto-rows: inherit;
+  align-items: center;
+  gap: 8px;
+  justify-content: flex-start;
+`;
+
+export const CustomRadioInput = styled.input`
+  place-content: center;
+  appearance: none;
+  background-color: black;
+  margin: 0;
+  width: 12px;
+  height: 12px;
+  border: 0.15em solid var(--text-color-button-grey);
+  border-radius: 50%;
+  transform: translateY(-0.075em);
+
+  &:checked {
+    background-color: rgba(227, 255, 168, 1);
+  }
 `;
 
 export const ActivityContainer = styled.div`
@@ -112,9 +136,37 @@ export const CancelButton = styled.button`
   }
 `;
 
-export const ErrorMessage = styled.p`
+export const ErrorText = styled.p`
   color: var(--input-border-color-error);
   font-size: 12px;
   line-height: 1.17;
   margin-top: 4px;
+`;
+
+export const AvatarImg = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  margin-right: 12px;
+  object-fit: cover;
+`;
+
+export const AvatarField = styled(Field)`
+  display: none;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const AvatarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const AvatarText = styled.p`
+  margin-left: 6px;
+  font-size: 14px;
+  line-height: 1.43;
 `;
