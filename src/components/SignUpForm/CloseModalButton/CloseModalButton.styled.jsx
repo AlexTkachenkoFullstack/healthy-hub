31951be008780b8fff44c2d0fa0299e05de8e6a7
barkode <konstantinks@gmail.com>
2023-09-24 00:02:10 +0100

    Update CloseModalButton.styled.jsx

--- src/components/SignUpForm/CloseModalButton/CloseModalButton.styled.jsx
+++ src/components/SignUpForm/CloseModalButton/CloseModalButton.styled.jsx
@@ -3,14 +3,4 @@ import styled from 'styled-components';
 export const CloseButton = styled.button`
   display: none;
   position: absolute;
-
-  @media (min-width: 834px) {
-    display: block;
-    position: absolute;
-    top: 20px;
-    right: 32px;
-    background: transparent;
-    border: none;
-    padding: 0;
-  }
 `;
