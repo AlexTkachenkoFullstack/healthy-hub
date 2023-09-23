ed738924662605ba2de361a4a059980cc0ee6905
AleksandrFedorov <ol.m.fed@gmail.com>
2023-09-23 14:12:48 +0300

    firstPush

---
+++ src/utils/DashBoard/TempData/YearWeightSet.js
@@ -0,0 +1,14 @@
+export const YearWeightSet = [
+  { data: 60, date: 'Jan' },
+  { data: 61, date: 'Feb' },
+  { data: 68, date: 'Mar' },
+  { data: 65, date: 'Apr' },
+  { data: 62, date: 'May' },
+  { data: 63, date: 'Jun' },
+  { data: 64, date: 'Jul' },
+  { data: 65, date: 'Aug' },
+  { data: 66, date: 'Sep' },
+  { data: 64, date: 'Oct' },
+  { data: 65, date: 'Nov' },
+  { data: 63, date: 'Dec' },
+];
