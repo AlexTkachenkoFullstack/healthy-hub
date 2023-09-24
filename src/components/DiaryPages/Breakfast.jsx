ea2196fd31a81999afd0c1b72877b2350021f33d
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-09-24 18:26:34 +0300

    123

--- src/components/DiaryPages/Breakfast.jsx
+++ src/components/DiaryPages/Breakfast.jsx
@@ -1,14 +1,24 @@
 
 import React from "react";
 import TitlePeriod from "./TitlePeriod";
-import sprite from '../../assets/images/icons/icons.svg';
+// import sprite from '../../assets/images/icons/icons.svg';
 import breakfastImg from '../../assets/images/meal-periods/breakfast.png';
+import { ButtonStyle, TextIndexSpan,  } from "./DiaryPage.styled";
+import Product from "./Product";
+
 
 const Breakfast = () => {
     return (
         <>
+        <img src={breakfastImg} alt="breakfast" />
         <h1>Breakfast</h1>
         <TitlePeriod />
+        <Product />
+        <ButtonStyle>
+      <TextIndexSpan>
+        + Record your meal
+      </TextIndexSpan>
+    </ButtonStyle>
         </>
     );
     };
