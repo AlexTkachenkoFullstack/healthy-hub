66c60273441432b435904e36742730455667e0da
barkode <konstantinks@gmail.com>
2023-09-24 01:48:20 +0100

    Update InputSuccessIcon.jsx

--- src/components/SignUpForm/InputSuccessIcon/InputSuccessIcon.jsx
+++ src/components/SignUpForm/InputSuccessIcon/InputSuccessIcon.jsx
@@ -1,10 +1,10 @@
-import closeIcon from '../../../assets/images/icons/correct.svg';
-import { InputSuccessIconStyle } from './InputSuccessIcon.styled';
+import icon from '../../../assets/images/icons/correct.svg';
+import { IconContainer } from './InputSuccessIcon.styled';
 
 export default function InputSuccessIcon() {
   return (
-    <InputSuccessIconStyle>
-      <img src={closeIcon} alt="Close button" width="16" height="16" />
-    </InputSuccessIconStyle>
+    <IconContainer>
+      <img src={icon} alt="Close button" width="16" height="16" />
+    </IconContainer>
   );
 }
