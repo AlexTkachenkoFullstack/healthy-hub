4b5ad90ceba8a906e1d2427843814fb179d040d4
barkode <konstantinks@gmail.com>
2023-09-25 00:54:05 +0100

    add button close to drop window

--- src/components/SignUpForm/CloseModalButton/CloseModalButton.styled.jsx
+++ src/components/SignUpForm/CloseModalButton/CloseModalButton.styled.jsx
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 
 export const CloseButton = styled.button`
   position: absolute;
+  border-color: transparent;
+  outline: none;
+  background-color: transparent;
+  right: 16px;
+  top: 16px;
 `;
