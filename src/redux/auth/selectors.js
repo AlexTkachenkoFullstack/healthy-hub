90fd72512db2d50fb47df734dcf94be83dcc6867
Nadiia Tsytsylina <miniova95@gmail.com>
2023-09-24 13:11:01 +0300

    add Loader, selectors, lazyLoading

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,5 @@
 export const getUserInfo = state => state.auth.user;
-export const getIsLoading = state => state.auth.isLoading;
+export const getIsLoadingAuth = state => state.auth.isLoading;
 export const getError = state => state.auth.error;
 export const getDateLastWeight = state => state.auth.dateLastWeight;
 export const getAuthStatus = state => !!state.auth.token;
