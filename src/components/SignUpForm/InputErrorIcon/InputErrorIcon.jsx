8ae23fdfd618945a7eaeb3b64fc16238785f45ed
barkode <konstantinks@gmail.com>
2023-09-24 18:50:31 +0100

    Update InputErrorIcon.jsx

--- src/components/SignUpForm/InputErrorIcon/InputErrorIcon.jsx
+++ src/components/SignUpForm/InputErrorIcon/InputErrorIcon.jsx
@@ -4,7 +4,7 @@ import { IconContainer } from './InputErrorIcon.styled';
 export default function InputErrorIcon() {
   return (
     <IconContainer>
-      <img src={icon} alt="Close button" width="16" height="16" />
+      <img src={icon} alt="Error" width="16" height="16" />
     </IconContainer>
   );
 }
