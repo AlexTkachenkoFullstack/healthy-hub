041c665f289ace0540eb248efc7ac3436eee042e
barkode <konstantinks@gmail.com>
2023-09-24 01:48:11 +0100

    Update PasswordClosedButtonIcon.jsx

--- src/components/SignUpForm/PasswordClosedButtonIcon/PasswordClosedButtonIcon.jsx
+++ src/components/SignUpForm/PasswordClosedButtonIcon/PasswordClosedButtonIcon.jsx
@@ -1,10 +1,10 @@
-import closeIcon from '../../../assets/images/icons/eyeClose.svg';
+import icon from '../../../assets/images/icons/eyeClose.svg';
 import { IconContainer } from './PasswordClosedButtonIcon.styled';
 
-export default function InputSuccessIcon() {
+export default function PasswordClosedButtonIcon() {
   return (
     <IconContainer>
-      <img src={closeIcon} alt="Close button" width="16" height="16" />
+      <img src={icon} alt="Close button" width="16" height="16" />
     </IconContainer>
   );
 }
