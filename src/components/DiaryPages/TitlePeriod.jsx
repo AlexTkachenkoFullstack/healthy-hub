49b4c5e9ae1dfe02e2ebefe1bb4a71885cbebb01
Petro Shutak <petro.shutak.ua@gmail.com>
2023-09-25 12:44:39 +0300

    Update TitlePeriod.jsx

--- src/components/DiaryPages/TitlePeriod.jsx
+++ src/components/DiaryPages/TitlePeriod.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import { IngredientSpan } from './DiaryPage.styled';
 
 const TitlePeriod = ({ product }) => {
+  if (product.length === 0 || product === undefined) {
+
+    return (
+      <>
+        <div style={{ display: 'inline' }}>
+          <IngredientSpan>Carbonohidrates: </IngredientSpan>
+          <IngredientSpan>0</IngredientSpan>
+        </div>
+        <div style={{ display: 'inline' }}>
+          <IngredientSpan>Protein: </IngredientSpan>
+          <IngredientSpan>0</IngredientSpan>
+        </div>
+        <div style={{ display: 'inline' }}>
+          <IngredientSpan>Fat: </IngredientSpan>
+          <IngredientSpan>0</IngredientSpan>
+        </div>
+      </>
+      );
+  }
+
   const sumCarbohydrates = product.reduce((acc, item) => {
     return acc + item.carbohydrates;
   }, 0);
