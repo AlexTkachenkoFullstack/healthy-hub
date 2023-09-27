a3571f74093323e699d77a696bb447e5d507418e
Tatiana-Tereshchenko <teresenkot33@gmail.com>
2023-09-27 18:31:53 +0300

    fixICON

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.jsx
@@ -1,13 +1,7 @@
 import React, { useState} from 'react';
 import Notification from '../../../src/components/Notification/Notification'
 import axios from 'axios';
-
 import { useNavigate } from 'react-router-dom';
-
-import { Formik, ErrorMessage } from 'formik';
-import InputSuccessIcon from '../../components/SignUpForm/InputSuccessIcon';
-import InputErrorIcon from '../../components/SignUpForm/InputErrorIcon';
-import { forgotSchema } from '../../components/SignInFormValidation/SignInFormValidation';
 import {
     ForgotContainer,
     ImageForgot,
@@ -20,14 +14,8 @@ import {
     TextBlock,
     TextSecond,
     TextSignUp,
-    IconTextPosition,
-    ValidationError,
-    InputBox,
 } from './ForgotPasswordPage.styled';
 
-const initialValues = {
-    email: '',
-};
 const ForgotPasswordPage = () => {
     const [email, setEmail] = useState('');
     const [isEmailValid, setIsEmailValid] = useState(true);
@@ -35,8 +23,6 @@ const ForgotPasswordPage = () => {
     const [notificationMessage, setNotificationMessage] = useState('');
     const [notificationColor, setNotificationColor] = useState('');
     const navigate = useNavigate();
-   
-
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
@@ -75,38 +61,19 @@ const ForgotPasswordPage = () => {
                 < ImageForgot />
             <ForgotText>
                     <ForgotTextTitle>Forgot your password</ForgotTextTitle>
-                <Text>We will send you an email with recovery instructions</Text>
-
-                <Formik initialValues={initialValues} onSubmit={handleSendClick} validationSchema={forgotSchema}>
-                    {({ errors, touched }) => (
-                        <FormForgot onSubmit={handleSendClick}>
-                             <InputBox htmlFor="email">   
-                            <TextField
-                                placeholder="E-mail"
-                              
-                                type='email'
-                                name="email"
-                                value={email}
-                                onChange={handleEmailChange}
-                            />
-                             <IconTextPosition>
-                                {errors.email && touched.email ? (
-                                <InputErrorIcon />
-                                ) : (
-                                <InputSuccessIcon />
-                                )}
-                            </IconTextPosition>
-                            </InputBox>
-                             {!isEmailValid && <div style={{ color: 'red' }}>{errorMessage}</div>}
-                            <ErrorMessage name="email">
-                                    {msg => <ValidationError>{msg}</ValidationError>}
-                                </ErrorMessage>
-                           
-                            <ButtonFogot type='submit' >Send</ButtonFogot>
-                        </FormForgot>
-                    )}
-                </Formik>
-
+                    <Text>We will send you an email with recovery instructions</Text>
+                    <FormForgot>
+                    <TextField
+                        placeholder="E-mail"
+                        label="email"
+                        type='email'
+                        name="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        />
+                        {!isEmailValid && <div style={{ color: 'red' }}>{errorMessage}</div>}
+                        <ButtonFogot  type='submit' onClick={handleSendClick}>Send</ButtonFogot>
+                    </FormForgot>
                     <TextBlock>
                         <TextSecond>Do you already have an account?</TextSecond>
                         <TextSignUp to="/signin">Sign in</TextSignUp>
